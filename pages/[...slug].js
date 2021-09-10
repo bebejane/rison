648e52739f6d6020e34b0ae9d3d50375ce089498
Bebe Jane <bjornthief@gmail.com>
2021-09-10 12:56:54 +0200

    fixing

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -46,7 +46,7 @@ export async function getStaticProps({params}) {
 export async function getStaticPaths() {
   const menu = await menuController.all()
   const dirs = fs.readdirSync(`${process.cwd()}/pages`, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
-  const paths = menu.filter(m => !dirs.includes(m.slug) && m.slug !== 'home').map((m)=>{ return {params:{slug:[m.slug]}}}).filter()
+  const paths = menu.filter(m => !dirs.includes(m.slug) && m.slug !== 'home').map((m)=>{ return {params:{slug:[m.slug]}}})
   return {
     paths,
     fallback: 'blocking'
