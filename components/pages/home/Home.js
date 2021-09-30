6f60193d21a91183f099a4434b505ce6f0f57b2a
Bebe Jane <bjornthief@gmail.com>
2021-09-30 15:57:20 +0200

    scroll arrow

--- components/pages/home/Home.js
+++ components/pages/home/Home.js
@@ -26,7 +26,7 @@ export default function Home({ page, contact, menu }) {
             </div>
             <div>
               <h3>This is how it works</h3>
-              <h3>→</h3>
+              <h3 className={styles.scrollArrow} onClick={handleScrollDown}>→</h3>
             </div>
           </div>
         </section>
