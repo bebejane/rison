9f1a7a672a72fef28a246a4fa3aa6ddab814e8a7
Bebe Jane <bjornthief@gmail.com>
2021-09-30 12:33:20 +0200

    fix contact

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
-import Head from "next/head";
-import React from "react";
-import Layout from "/components/common/Layout";
 import "../styles/index.scss";
+import Head from "next/head";
+import Layout from "/components/common/Layout";
 import { Provider } from "/lib/context";
+import { AnimatePresence } from 'framer-motion';
 
-const MyApp = ({ Component, pageProps, menu }) => (  
+const MyApp = ({ Component, pageProps, router, menu }) => (  
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     </Head>  
     <Provider>
       <Layout>
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route}/>
+        </AnimatePresence>
       </Layout>
     </Provider>
   </>
