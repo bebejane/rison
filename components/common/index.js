73eeb93a3c3f1ff97f01bb9e592b6828f83fc625
Bebe Jane <bjornthief@gmail.com>
2021-10-01 10:31:01 +0200

    common exports

---
+++ components/common/index.js
@@ -0,0 +1,6 @@
+import Button from './Button'
+import SectionFollow from './SectionFollow'
+export {
+  Button,
+  SectionFollow
+}
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,9 +1,15 @@
 import Home from '/components/pages/home'
 import {homeController} from "/controllers";
+import { apiQuery } from '/lib/api';
+import { GetHome } from '/graphql/home';
+import { GetMenu } from '/graphql/menu';
+import { GetContact } from '/graphql/contact';
 
 export default Home;
 
 export async function getStaticProps({preview}) {
   const data = await homeController.get(preview)
+  const test = await apiQuery([GetHome, GetMenu, GetContact], [], preview)
+  console.log(test)
   return { props: {...data}, revalidate:10 }
 }
