0419dfd84496f75f6c5b89d59e3290e5fbbcdbd4
Bebe Jane <bjornthief@gmail.com>
2021-10-01 14:22:14 +0200

    testtt

--- components/common/Content.js
+++ components/common/Content.js
@@ -2,22 +2,23 @@ import Head from "next/head";
 import styles from "./Content.module.scss";
 import {Footer, NavBar, Contact} from "./";
 import { motion } from "framer-motion";
+const duration = 5;
 
 const pageAnimation = {
 	initial: {
 		//x: "100%",
 		opacity: 1,
-		transition: { duration: 0.3, delay: 0.0 },
+		transition: { duration, delay: 0.0 },
 	},
 	animate: {
 		//x: "0%",
 		opacity: 1,
-		transition: { duration: 0.3 },
+		transition: { duration },
 	},
 	exit: {
 		//x: "-100%",
 		opacity: 0,
-		transition: { duration: 0.3, delay: 0.0 },
+		transition: { duration, delay: 0.0 },
 	},
 };
 
@@ -28,7 +29,7 @@ export default function Content({ page = {}, contact, menu, children }) {
 				<title>Rison {page.title ? ` | ${page.title}` : ""}</title>
 				<meta name="description" content={page.description} />
 			</Head>
-			<motion.div initial="exit" animate="animate" exit="exit" variants={pageAnimation}>
+			<motion.div initial="initial" animate="animate" exit="exit" variants={pageAnimation}>
 				<div className={styles.content}>
 					<NavBar menu={menu} />
 					<div key={'content'} className={styles.main}>{children}</div>
