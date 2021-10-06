c88a76d657eb19af242127314090802502b269b5
konst-teknik <information-konst-teknik.se>
2021-10-06 12:44:18 +0200

    ny about

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,18 +1,22 @@
 import styles from "./About.module.scss";
 import { Content, Markdown, SectionFollow, Button, SectionImageHeadline } from "/components/common";
 import { apiQuery } from "/lib/api";
-import { GetHome, GetMenu, GetContact } from "/graphql";
+import { GetAbout, GetMenu, GetContact } from "/graphql";
 import { useRef } from "react";
 import { useUI, UIAction } from "/lib/context/ui";
 
 export default function About({page, contact, menu}) {
+	const { intro, people } = page;
+
 	return (
-      <h2>About content</h2>
+    <section className={styles.intro}>
+    <h2>{page.intro}</h2>
+  </section>
 	);
 }
 
 export async function getStaticProps({ preview }) {
-  const queries = [GetMenu, GetContact];
+  const queries = [GetAbout,GetMenu, GetContact];
   const data = await apiQuery(queries, {}, preview);
 	return { props: { ...data }, revalidate: parseInt(process.env.REVALIDATE_TIME) };
 }
