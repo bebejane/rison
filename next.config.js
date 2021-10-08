ad820fe4f1c55e10dcb760282a0e8ff906ddf895
Bebe Jane <bjornthief@gmail.com>
2021-10-08 12:43:23 +0200

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withGraphql = require('next-plugin-graphql')
 const withTM = require('next-transpile-modules')(['seamless-scroll-polyfill']);
+const withBundleAnalyzer = require('@next/bundle-analyzer')({enabled: process.env.ANALYZE === 'true'})
 const sassOptions = {
   includePaths: ['./components', './pages'],
   prependData: `
@@ -13,9 +14,10 @@ const sassOptions = {
 const nextOptions = {
   devIndicators: {
     buildActivity: false
-  }
+  },
+  distDir: 'build'
 }
 
-const config = withPlugins([withGraphql, withTM], {sassOptions, ...nextOptions})
+const config = withPlugins([withBundleAnalyzer, withGraphql, withTM], {sassOptions, ...nextOptions})
 
 module.exports = config
