b42dd3a1db5e14f99fb59f23db215a94f47735da
Bebe Jane <bjornthief@gmail.com>
2021-10-08 12:47:01 +0200

    fix build dir again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withGraphql = require('next-plugin-graphql')
 const withTM = require('next-transpile-modules')(['seamless-scroll-polyfill']);
-const withBundleAnalyzer = require('@next/bundle-analyzer')({enabled: process.env.ANALYZE === 'true'})
+//const withBundleAnalyzer = require('@next/bundle-analyzer')({enabled: process.env.ANALYZE === 'true'})
 const sassOptions = {
   includePaths: ['./components', './pages'],
   prependData: `
@@ -14,10 +14,9 @@ const sassOptions = {
 const nextOptions = {
   devIndicators: {
     buildActivity: false
-  },
-  distDir: 'out'
+  }
 }
 
-const config = withPlugins([withBundleAnalyzer, withGraphql, withTM], {sassOptions, ...nextOptions})
+const config = withPlugins([withGraphql, withTM], {sassOptions, ...nextOptions})
 
 module.exports = config
