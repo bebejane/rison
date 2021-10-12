c703d98b79e7df37135b9b593bd9d819070a5901
Bebe Jane <bjornthief@gmail.com>
2021-10-12 13:36:04 +0200

    fixed seo url tag

--- graphql/index.js
+++ graphql/index.js
@@ -5,4 +5,3 @@ export { default as GetOurOffer } from "./ourOffer.graphql";
 export { default as GetAbout } from "./about.graphql";
 export { default as GetPartner } from "./partner.graphql";
 export { default as GetInvest } from "./invest.graphql";
-
