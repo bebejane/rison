cfa650c2ba92adc7e329b9b4c0d02c917f5679cf
Bebe Jane <bjornthief@gmail.com>
2021-10-17 19:56:11 +0200

    contact overflow

--- components/common/SectionFollowBlock.js
+++ components/common/SectionFollowBlock.js
@@ -8,16 +8,16 @@ import useVisibility from "/lib/hooks/useVisibility";
 
 const SectionFollowBlock = ({ block, odd }) => {
 	const [ref, { ratio, direction, wasVisible, wasPassed }] = useVisibility('follow', 0, 100);
-	const height = wasPassed ? 100 : ratio < 1 ? ratio*80 : ratio*100
+	const height = wasPassed ? 100 : ratio < 1 ? ratio*75 : ratio*100
   
 	return (
 		<div className={cn(styles.wrapper)} ref={ref}>
 			<article>
-				<Reveal effect="fadeUp" >
+				<Reveal effect="fadeUp" distance={20} duration={1000} fade={true}>	
 					<h3 className="tight">{block.shortHeadline}</h3>
 					<h2 className="tight">{block.headline}</h2>
 					<p><Markdown>{block.text}</Markdown></p>
-				</Reveal>
+				</Reveal>	
 				<div className={styles.line} style={{height:`${height}%`}} ></div>
 			</article>
 			<figure>{block.image && <Image data={block.image.responsiveImage} />}</figure>
--- components/fx/Reveal.js
+++ components/fx/Reveal.js
@@ -4,21 +4,49 @@ import useVisibility from "/lib/hooks/useVisibility";
 
 const effects = {
   fadeUp : {
-    duration : 1000    
+    duration : 1000,
+    transform: 'translateY',
+    distance : 50,
+    direction : '+',
+    unit: 'px',
+    fade:true
+  },
+  slideInLeft : {
+    duration : 1000,
+    transform: 'translateX',
+    distance : 100,
+    direction : '-',
+    unit: 'px',
+    fade:true
+  },
+  slideInRight: {
+    duration : 1000,
+    transform: 'translateX',
+    distance : 100,
+    direction : '+',
+    unit: 'px',
+    fade:true
   }
 }
 
 const Reveal = (props) => {
+  const effect = effects[props.effect] && styles[props.effect] ? effects[props.effect] : undefined
+  if(!effect) 
+    return <div>{`Effect "${props.effect}" doesn't exist!`}</div>
 
-  const { 
-    effect = "fadeUp",  
-    duration,
-    steps = 100,
-    threshhold = 0
-  } = props
+  const { steps = 100, threshhold = 0} = props
+  const duration = props.duration || effect.duration
+  const distance = Math.abs(props.distance || effect.distance)
+  const fade = props.fade === false ? false : effect.fade
 
-	const [visRef, { ratio, direction, wasVisible, wasPassed }] = useVisibility('', threshhold, steps);	
-  const effectClass = ratio > 0 || wasVisible  ? styles[effect] : false
+  const [visRef, { ratio, direction, wasVisible, wasPassed }] = useVisibility('', threshhold, steps);	
+  const effectClass = ratio > 0 || wasVisible  ? styles[props.effect] : undefined
+
+  const style = {
+    animationDuration:`${(duration)/1000}s`,
+    transform : effect.transform ? `${effect.transform}(${effect.direction}${distance}${effect.unit})` : undefined,
+    opacity : fade ? 0 : 1
+  }
   
 	return (
     <>
@@ -26,9 +54,7 @@ const Reveal = (props) => {
           const props = {
             ref: idx === 0 ? visRef : null,
             className:effectClass,
-            style:{
-              animationDuration:`${duration/1000}s`,
-            }
+            style
           }
           return React.cloneElement(child, props)
         })
@@ -36,5 +62,11 @@ const Reveal = (props) => {
     </>
   )
 };
+
+
+const generateStyles = (props) => {
+
+
+}
 
 export default Reveal;
