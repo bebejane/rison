73227158be0db643c2875ab7fa6b4643d50b0e62
Bebe Jane <bjornthief@gmail.com>
2021-10-19 16:04:10 +0200

    fix navbar glitch again

--- components/common/NavBar.js
+++ components/common/NavBar.js
@@ -1,17 +1,18 @@
 import styles from './NavBar.module.scss';
 import Link from 'next/link';
 import cn from 'classnames';
+import Hamburger from 'hamburger-react';
 import { useUI, UIAction } from '/lib/context/ui';
 import { useEffect, useState } from 'react';
 import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import { useWindowSize } from 'rooks'
-import Hamburger from 'hamburger-react';
 
 const menuHeight = 100;
 const scrollBreakpoint = 980
+const initialScrolStyles = { ratio: 0, marker: 0, direction:undefined }
 
 const generateScrollStyles = (ratio, direction, marker) => {
-	const opacity = Math.max(1 - (ratio*1.8), 0);
+	const opacity = Math.max(1 - (ratio*1), 0);
 	const scale = Math.max(1, 1 + ratio - 0.75);
 	const margin = 100
 	return {
@@ -19,7 +20,7 @@ const generateScrollStyles = (ratio, direction, marker) => {
 		i: { transform: `translateX(-${ratio * margin}px) translateY(${ratio * margin}px)`, opacity },
 		s: { transform: `translateX(-${ratio * margin}px) translateY(${ratio * margin}px)`, opacity },
 		o: { transform: `translateX(-${ratio * 95}px) scale(${scale})`, top:ratio*100},
-		n: { transform: `translateY(-${ratio * margin}px)`, opacity:opacity },
+		n: { transform: `translateY(${(ratio/3) * margin}px)`, opacity },
 		nav: { transform: `translateY(-${ratio * margin}%)` },
 		ratio,
 		direction,
@@ -31,26 +32,24 @@ const generateScrollStyles = (ratio, direction, marker) => {
 export default function NavBar({ menu, contact, pathname }) {
 	const [ui, setUI] = useUI();
 	const [showMobileMenu, setShowMobileMenu] = useState(false);
-	const [scrollStyles, setScrollStyles] = useState({ ratio: 0, marker: 0, direction:undefined});
+	const [scrollStyles, setScrollStyles] = useState(initialScrolStyles);
 	const { innerWidth: windowWidth } = useWindowSize();
 
 	const enableFloater = () => {
 		if(!scrollStyles.floater) return // Dont trigger until scrolled down
-
 		const speed = 200;
 		const marker = -window.scrollY
 		let ratio = 1;
-		const i = setInterval(()=>{
-			if(ratio <= 0) return clearInterval(i)
-			ratio = Math.max(ratio-=0.02, 0)
-			setScrollStyles(generateScrollStyles(ratio, 'up', marker));
+		const loop = setInterval(()=>{
+			if(ratio <= 0) return clearInterval(loop)
+			setScrollStyles(generateScrollStyles(ratio = Math.max(ratio-=0.02, 0), 'down', marker));
 		}, speed/100)
 	};
 
 	useScrollPosition(({ prevPos, currPos }) => {
 		
-		if (windowWidth < scrollBreakpoint) return setScrollStyles({}); // Skip and reset below desktop
-		if (currPos.y === 0) return setScrollStyles({}); // Reset on reload
+		if (windowWidth < scrollBreakpoint) return setScrollStyles(initialScrolStyles); // Skip and reset below desktop
+		if (currPos.y === 0) return setScrollStyles(initialScrolStyles); // Reset on reload
 		if (currPos.y > 0) return console.log('bounce'); // Ignore on bounce scroll Safari
 		
 		const { y } = currPos;
@@ -59,7 +58,6 @@ export default function NavBar({ menu, contact, pathname }) {
 		const marker = prevDirection != direction ? y - (direction === 'up' ?  ((1-prevRatio)*menuHeight) : 0) : prevMarker;
 		const ratio = direction === 'down' ? Math.min(Math.abs(y - marker) / menuHeight, 1) : (Math.min(Math.abs(marker), 100) - Math.min(y - marker, 100)) / 100;
 		setScrollStyles(generateScrollStyles(ratio, direction, marker));
-		
 	});
 	useEffect(() => windowWidth < scrollBreakpoint && setScrollStyles({}), [windowWidth]) // Fix resize bug
 
