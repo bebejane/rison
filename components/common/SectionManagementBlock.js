d4472dcfa5d737f3a67efdccf8b13c494843cbf3
konst-teknik <information-konst-teknik.se>
2021-11-08 22:01:09 +0100

    Section Management Anim

--- components/common/SectionManagementBlock.js
+++ components/common/SectionManagementBlock.js
@@ -5,17 +5,17 @@ import Reveal from "components/fx/Reveal";
 
 const SectionManagementBlock = ({ block, odd }) => (
   <div className={styles.people}>
+    <Reveal effect="fadeUp" delay={!odd ? 300 : 0}>
       <figure>
         {block.image && <Image lazyLoad={false} data={block.image.responsiveImage} />}
       </figure>
-      <Reveal effect="fadeUp" delay={!odd ? 300 : 0}>
-        <article>
-          <p><strong>{block.name}</strong></p>
-          <p>{block.title}</p>
-          <p><a href={'mailto:' + block.email}>{block.email}</a></p>
-          <p>{block.text}</p>
-        </article>
-      </Reveal>
+      <article>
+        <p><strong>{block.name}</strong></p>
+        <p>{block.title}</p>
+        <p><a href={'mailto:' + block.email}>{block.email}</a></p>
+        <p>{block.text}</p>
+      </article>
+    </Reveal>
   </div>
 );
 
